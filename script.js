a09b19c7749f7d2bb3ddc8c51f7cb8aefedce04d
Glimpz <abgabbard@gmail.com>
2021-01-13 17:11:30 -0700

    there were some errors persisting in the previous commit, now all save buttons working

--- script.js
+++ script.js
@@ -6,35 +6,35 @@ $(document).ready(() => {
     });
     $("#save02").click(() => {
         var userInput02 = $("#block02").val()
-        localStorage.setItem("usterStored02", userInput02 )
+        localStorage.setItem("userStored02", userInput02 )
     });
     $("#save03").click(() => {
         var userInput03 = $("#block03").val()
-        localStorage.setItem("usterStored03", userInput01 )
+        localStorage.setItem("userStored03", userInput03 )
     });
     $("#save04").click(() => {
         var userInput04 = $("#block04").val()
-        localStorage.setItem("usterStored04", userInput02 )
+        localStorage.setItem("userStored04", userInput04 )
     });
     $("#save05").click(() => {
         var userInput05 = $("#block05").val()
-        localStorage.setItem("usterStored05", userInput01 )
+        localStorage.setItem("userStored05", userInput05 )
     });
     $("#save06").click(() => {
         var userInput06 = $("#block06").val()
-        localStorage.setItem("usterStored06", userInput02 )
+        localStorage.setItem("userStored06", userInput06 )
     });
     $("#save07").click(() => {
         var userInput07 = $("#block07").val()
-        localStorage.setItem("usterStored07", userInput01 )
+        localStorage.setItem("userStored07", userInput07 )
     });
     $("#save08").click(() => {
         var userInput08 = $("#block08").val()
-        localStorage.setItem("usterStored08", userInput02 )
+        localStorage.setItem("userStored08", userInput08 )
     });
     $("#save09").click(() => {
         var userInput09 = $("#block09").val()
-        localStorage.setItem("usterStored09", userInput02 )
+        localStorage.setItem("userStored09", userInput09 )
     });
 
 });
